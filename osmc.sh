93803cd017a8d3e16d4d5f261de82c03b159420a
FullMentalPanic <ganliang0122@gmail.com>
2018-08-12 23:07:16 +0800

    shell control ok

--- osmc.sh
+++ osmc.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sleep 10s
 sudo mount /dev/sda1 /home/osmc/hdd/
+sudo service transmission-daemon start
 source /home/osmc/script/env/bin/activate
 cd /home/osmc/script/PiSpider/
 pwd
-python3 main.py
+sudo python3 main.py

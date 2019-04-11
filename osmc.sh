66deae192c6eb499e137ed504716282f0564b7bf
FullMentalPanic <ganliang0122@gmail.com>
2019-04-11 22:59:37 +0800

    update shell script

--- osmc.sh
+++ osmc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git pull https://github.com/FullMentalPanic/spider_in_osmc/blob/master/PiSpider/PiSpider/config.yml
+git pull /spider_in_osmc/PiSpider/PiSpider/config.yml
 cd /spider_in_osmc/PiSpider/
 pwd
 python3 main.py

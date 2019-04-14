9b73ff46aa76dcb1ab391f95b92553a480fc69e6
FullMentalPanic <ganliang0122@gmail.com>
2019-04-14 16:52:27 +0800

    fix location bug

--- PiSpider/PiSpider/transmission.py
+++ PiSpider/PiSpider/transmission.py
@@ -28,9 +28,9 @@ class Transmission_control(object):
         #self.tc.add_torrent(torrent,download_dir=abs_path)
         transmission_add_torrent = ['/usr/bin/transmission-remote', "-n", "transmission:transmission",]
         transmission_add_torrent.append("--add")
-        transmission_add_torrent.append(torrent)
+        transmission_add_torrent.append((torrent)
         transmission_add_torrent.append("-w")
-        transmission_add_torrent.append(location)
+        transmission_add_torrent.append(abs_path)
         subprocess.call(transmission_add_torrent)
 
     def creat_folder(self, dir):
